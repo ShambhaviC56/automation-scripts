32ad566e05e2303e70cefb9a3df398a26a1654f8
ShambhaviC56 <Shambhavi.Chandel@jadeglobal.com>
2026-01-13 21:02:20 +0530

    Add Playwright JavaScript automation script: TC001

---
+++ automation-scripts/Playwright/JavaScript/AccountLocators.js
@@ -0,0 +1,13 @@
+module.exports = {
+  newButton: "button[name='New']",
+  accountNameInput: "lightning-input-field[field-name='Name'] input",
+  accountTypeCombobox: "lightning-combobox[field-name='Type'] button",
+  accountIndustryCombobox: "lightning-combobox[field-name='Industry'] button",
+  accountNumberInput: "lightning-input-field[field-name='AccountNumber'] input",
+  accountSiteInput: "lightning-input-field[field-name='Site'] input",
+  saveButton: "button[name='SaveEdit']",
+  successToast: "div.forceToastMessage",
+  accountTypeOption: "lightning-base-combobox-item",
+  accountIndustryOption: "lightning-base-combobox-item",
+  successMessage: "div.forceToastMessage span.toastMessage"
+};
