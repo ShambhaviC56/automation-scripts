1a13f7f8579e39196996363f316abb7d071981a8
ShambhaviC56 <Shambhavi.Chandel@jadeglobal.com>
2026-01-13 21:14:40 +0530

    Add Playwright JavaScript automation script: TC003

--- automation-scripts/Playwright/JavaScript/AccountPage.js
+++ automation-scripts/Playwright/JavaScript/AccountPage.js
@@ -1,40 +1,41 @@
 const BasePage = require('./BasePage');
 const locators = require('./AccountLocators');
-const { click, fill, waitForElement, getText, selectComboboxOption } = require('./utils/CommonActions');
+const { click, fill, waitForElement, getText } = require('./utils/CommonActions');
 
 class AccountPage extends BasePage {
   constructor(page) {
     super(page);
-    this.page = page;
   }
 
-  async navigateToAccountsTab() {
-    // Implementation depends on your application's navigation
-    // This might involve clicking a menu item or navigating to a specific URL
-    console.log('Navigating to Accounts Tab - Implementation Required');
-  }
-
-  async createAccount(accountName, accountNumber, accountType, accountIndustry, accountSite) {
+  async createAccountWithInvalidData(accountName, website) {
     await click(this.page, locators.newButton);
     await fill(this.page, locators.accountNameInput, accountName);
-    await fill(this.page, locators.accountNumberInput, accountNumber);
-
-    await click(this.page, locators.accountTypeCombobox);
-    await this.page.locator(locators.accountTypeOption, {hasText: accountType}).click();
-
-    await click(this.page, locators.accountIndustryCombobox);
-    await this.page.locator(locators.accountIndustryOption, {hasText: accountIndustry}).click();
-
-    await fill(this.page, locators.accountSiteInput, accountSite);
-
+    await fill(this.page, locators.websiteInput, website);
     await click(this.page, locators.saveButton);
-    await waitForElement(this.page, locators.successToast);
   }
 
-  async getSuccessMessage() {
-    await this.page.waitForSelector(locators.successMessage);
-    return await this.page.locator(locators.successMessage).innerText();
+  async getAccountNameErrorMessage() {
+    await waitForElement(this.page, locators.accountNameError);
+    return await getText(this.page, locators.accountNameError);
   }
+
+  async getWebsiteErrorMessage() {
+    await waitForElement(this.page, locators.websiteError);
+    return await getText(this.page, locators.websiteError);
+  }
+
+  async isErrorMessageDisplayed() {
+    return await this.page.locator(locators.errorMessage).isVisible();
+  }
+
+  async isAccountNameInputPopulated() {
+        return await this.page.locator(locators.accountNameInput).inputValue();
+  }
+
+  async isWebsiteInputPopulated() {
+        return await this.page.locator(locators.websiteInput).inputValue();
+  }
+
 }
 
 module.exports = AccountPage;
