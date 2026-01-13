63b92ffd756e8e3cb494736f003675d75ea3ed6e
ShambhaviC56 <Shambhavi.Chandel@jadeglobal.com>
2026-01-13 21:14:39 +0530

    Add Playwright JavaScript automation script: TC003

--- automation-scripts/Playwright/JavaScript/AccountLocators.js
+++ automation-scripts/Playwright/JavaScript/AccountLocators.js
@@ -1,13 +1,9 @@
 module.exports = {
   newButton: "button[name='New']",
   accountNameInput: "lightning-input-field[field-name='Name'] input",
-  accountTypeCombobox: "lightning-combobox[field-name='Type'] button",
-  accountIndustryCombobox: "lightning-combobox[field-name='Industry'] button",
-  accountNumberInput: "lightning-input-field[field-name='AccountNumber'] input",
-  accountSiteInput: "lightning-input-field[field-name='Site'] input",
+  websiteInput: "lightning-input-field[field-name='Website'] input[type='url']",
   saveButton: "button[name='SaveEdit']",
-  successToast: "div.forceToastMessage",
-  accountTypeOption: "lightning-base-combobox-item",
-  accountIndustryOption: "lightning-base-combobox-item",
-  successMessage: "div.forceToastMessage span.toastMessage"
+  errorMessage: "div.forceToastMessage",
+  accountNameError: "lightning-input-field[field-name='Name'] div.slds-form-element__help",
+  websiteError: "lightning-input-field[field-name='Website'] div.slds-form-element__help"
 };
