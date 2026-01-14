6a33344f524832f6f4eec61ddd3ff054bc575154
ShambhaviC56 <Shambhavi.Chandel@jadeglobal.com>
2026-01-14 10:51:59 +0530

    Add Playwright JavaScript automation script: TC106

---
+++ playwright/javascript/locators/QuoteLocators.js
@@ -0,0 +1,14 @@
+module.exports = {
+  newButton: "div[title='New']",
+  quoteNameInput: "lightning-input-field[field-name='Name'] input",
+  accountLookup: "lightning-lookup[field-name='AccountId'] input",
+  accountLookupResult: "lightning-base-combobox-item",
+  saveButton: "button[name='SaveEdit']",
+  cancelButton: "button[name='CancelEdit']",
+  quotePageHeader: ".slds-page-header",
+  editButton: "button[name='Edit']",
+  deleteButton: "button[name='Delete']",
+  createQuotePageUrlCheck: '/lightning/o/Quote/new',
+  createDropdown: "button[title='Create...']",
+  quotesOption: "a[title='Quotes']"
+};
