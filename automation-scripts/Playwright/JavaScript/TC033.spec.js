5c803064f86fd99f1ff03fed30eaeaba92814f13
ShambhaviC56 <Shambhavi.Chandel@jadeglobal.com>
2026-01-22 05:16:26 +0530

    Add test for contact creation timeout in Salesforce
    This test simulates a timeout scenario during contact creation in Salesforce.

---
+++ automation-scripts/Playwright/JavaScript/TC033.spec.js
@@ -0,0 +1,33 @@
+const { test } = require('@playwright/test');
+const path = require('path');
+
+test.use({
+  storageState: path.join(__dirname, '../storageState.json'),
+});
+
+test('FAIL: Contact creation timeout', async ({ page }) => {
+
+  test.setTimeout(10000);
+
+  // Open Contacts list
+  await page.goto(
+    'https://orgfarm-5694adb5bf-dev-ed.develop.lightning.force.com/lightning/o/Contact/list'
+  );
+
+  await page.click('a[title="New"]');
+
+  await page.fill(
+    '//label[text()="Last Name"]/following::input[1]',
+    'TimeoutTest'
+  );
+
+  // ❌ INTENTIONAL TIMEOUT
+  // Salesforce save spinner / button never completes within 2s
+  await page.click('button[name="SaveEdit"]');
+
+  await page.waitForSelector(
+    'span.toastMessage',
+    { timeout: 2000 }
+  );
+
+});
