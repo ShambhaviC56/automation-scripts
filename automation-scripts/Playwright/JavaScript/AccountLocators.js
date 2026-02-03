20d95f73ab30ce2fa4630b023243bd806270b2d9
ShambhaviC56 <Shambhavi.Chandel@jadeglobal.com>
2026-02-03 20:37:46 +0530

    Delete automation-scripts directory

--- automation-scripts/Playwright/JavaScript/AccountLocators.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  newButton: "button[name='New']",
-  accountNameInput: "lightning-input-field[field-name='Name'] input",
-  websiteInput: "lightning-input-field[field-name='Website'] input[type='url']",
-  saveButton: "button[name='SaveEdit']",
-  errorMessage: "div.forceToastMessage",
-  accountNameError: "lightning-input-field[field-name='Name'] div.slds-form-element__help",
-  websiteError: "lightning-input-field[field-name='Website'] div.slds-form-element__help"
-};
